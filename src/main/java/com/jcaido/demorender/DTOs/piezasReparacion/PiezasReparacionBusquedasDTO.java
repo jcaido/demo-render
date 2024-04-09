6fba74350017b6fe17ea41d26c551c79df2b729d
Cristobal Rosa <crisrogar@gmail.com>
2024-04-09 12:46:15 +0200

    update PiezasReparacionBusquedasDTO

--- src/main/java/com/jcaido/demorender/DTOs/piezasReparacion/PiezasReparacionBusquedasDTO.java
+++ src/main/java/com/jcaido/demorender/DTOs/piezasReparacion/PiezasReparacionBusquedasDTO.java
@@ -1,5 +1,6 @@
 package com.jcaido.demorender.DTOs.piezasReparacion;
 
+import com.jcaido.demorender.models.OrdenReparacion;
 import com.jcaido.demorender.models.Pieza;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,5 +16,5 @@ public class PiezasReparacionBusquedasDTO {
     private Long id;
     private Integer cantidad;
     private Pieza pieza;
-    //private OrdenReparacion ordenReparacion;
+    private OrdenReparacion ordenReparacion;
 }
